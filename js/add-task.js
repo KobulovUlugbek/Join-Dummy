a9a0f93930197ad1e208919ca6bf208e6b7fd469
Ulugbek Kobulov <108155304+KobulovUlugbek@users.noreply.github.com>
2023-05-19 20:15:32 +0200

    second comment

---
+++ js/add-task.js
@@ -0,0 +1,8 @@
+let popup = document.getElementById('header-popup');
+
+function openPopup(){
+    popup.classList.add('open-popup');
+}
+function closePopup(){
+    popup.classList.remove('open-popup');
+}
