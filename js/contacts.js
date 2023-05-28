c2493273c91ebb92a55100399424882821aeeb03
NickyHo16 <nicole.hollaender74@gmail.com>
2023-05-28 09:32:52 +0200

    add JSON

---
+++ js/contacts.js
@@ -0,0 +1,32 @@
+let contacts = [
+    {
+        'firstName': 'Ulugbek',
+        'lastName': 'Kobulov',
+        'email': 'kobulov@web.de',
+        'phone': '+49 123 567 891',
+    },
+    {
+        'firstName': 'Nils',
+        'lastName': 'Scholz',
+        'email': 'scholz@web.de',
+        'phone': '+49 123 567 891',
+    },
+    {
+        'firstName': 'Sascha',
+        'lastName': 'Tichy',
+        'email': 'tichy@web.de',
+        'phone': '+49 123 567 891',
+    },
+    {
+        'firstName': 'Sylvia',
+        'lastName': 'Zartmann',
+        'email': 'zartmann@web.de',
+        'phone': '+49 123 567 891',
+    },
+    {
+        'firstName': 'Nicole',
+        'lastName': 'Holländer',
+        'email': 'hollaender@web.de',
+        'phone': '+49 123 567 891',
+    },
+]
