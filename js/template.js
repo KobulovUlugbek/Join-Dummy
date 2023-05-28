4db5838e3d9a0b1ed190cd4baa72e2329be987e4
Ulugbek Kobulov <108155304+KobulovUlugbek@users.noreply.github.com>
2023-05-28 20:24:33 +0200

    Revert "Merge branch 'main' of github.com:KobulovUlugbek/Join-Dummy"
    This reverts commit 31a61e2abdc4bf16547ab7c5c18bf001254cf5ae, reversing
    changes made to 8083ce0697ff6b73658df4d10e6610db894622d7.

--- js/template.js
+++ js/template.js
@@ -1,12 +1,8 @@
 fetch('header.html')
-    .then((response) => response.text())
-    .then((template) => {
-        document.getElementById('header-template').innerHTML = template;
-    })
-    .catch((error) => {
-        console.error('Error fetching template:', error);
-    });
-
-function showLogoutButton() {
-    document.getElementById('logout-container').classList.toggle('d-none');
-}
+  .then(response => response.text())
+  .then(template => {
+    document.getElementById('header-template').innerHTML = template;
+  })
+  .catch(error => {
+    console.error('Error fetching template:', error);
+  });
