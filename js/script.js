f2c1b762372d34fa34953aada000f63c84e5cc4a
niels-2005 <niels.scholz01@web.de>
2023-05-31 18:14:11 +0200

    added filter for titles

--- js/script.js
+++ js/script.js
@@ -1,35 +1,48 @@
 // BOARD drag and drop
-let todos = [{
-    'id': 0,
-    'category' : 'Design',
-    'situation': 'todo',
-    'discription': 'Modify the contens of the main website...',
-    'team': 'contactA'
-},{
-    'id': 1,
-    'category' : 'Sales',
-    'situation': 'progress',
-    'discription': 'Modify the contens of the main website...',
-    'team': 'contactA'
-},{
-    'id': 2,
-    'category' : 'Backoffice',
-    'situation': 'awaiting',
-    'discription': 'Modify the contens of the main website...',
-    'team': 'contactA'
-},{
-    'id': 3,
-    'category' : 'Marketing',
-    'situation': 'done',
-    'discription': 'Modify the contens of the main website...',
-    'team': 'contactA'
+let todos = [
+    {
+        id: 0,
+        title: 'Website redesign',
+        category: 'Design',
+        situation: 'todo',
+        discription: 'Modify the contens of the main website...',
+        team: 'contactA',
+    },
+    {
+        id: 1,
+        title: 'Website redesign',
+        category: 'Sales',
+        situation: 'progress',
+        discription: 'Modify the contens of the main website...',
+        team: 'contactA',
+    },
+    {
+        id: 2,
+        title: 'Join Tasks',
+        category: 'Backoffice',
+        situation: 'awaiting',
+        discription: 'Modify the contens of the main website...',
+        team: 'contactA',
+    },
+    {
+        id: 3,
+        title: 'Meow',
+        category: 'Marketing',
+        situation: 'done',
+        discription: 'Modify the contens of the main website...',
+        team: 'contactA',
+    },
+];
+
+function init() {
+    dragDrop();
+    document.getElementById('filterInput').addEventListener('keyup', filterTasks);
 }
-]
 
 function dragDrop() {
     const situations = ['todo', 'progress', 'awaiting', 'done'];
     for (const situation of situations) {
-        const filteredTodos = todos.filter(t => t['situation'] === situation);
+        const filteredTodos = todos.filter((t) => t['situation'] === situation);
         const containerElement = document.getElementById(situation);
         containerElement.innerHTML = '';
         for (const element of filteredTodos) {
@@ -42,7 +55,7 @@ function generateTodoHTML(element) {
     return `
     <div id="task${element['id']}" draggable="true" ondragstart="startdragging(${element['id']})" class="child-container">
         <div class="${element['category']}">${element['category']}</div>
-        <p>Website redesign</p>
+        <p>${element['title']}</p>
         <h4>${element['discription']}</h4>
         <div class="progressbar-container">
             <div class="progressbar"></div>
@@ -58,12 +71,12 @@ function generateTodoHTML(element) {
                 <img src="img/urgent-icon4.png" alt="" />
             </div>
         </div>
-    </div>`
+    </div>`;
 }
 
 let currentlyDraggedElement;
 
-function startdragging(id){
+function startdragging(id) {
     currentlyDraggedElement = id;
 }
 
@@ -74,4 +87,18 @@ function allowDrop(ev) {
 function changePos(situation) {
     todos[currentlyDraggedElement]['situation'] = situation;
     dragDrop();
+}
+
+function filterTasks() {
+    let filterValue = document.getElementById('filterInput').value.toUpperCase();
+
+    for (let i = 0; i < todos.length; i++) {
+        let taskElement = document.getElementById('task' + todos[i].id);
+
+        if (todos[i].title.toUpperCase().includes(filterValue)) {
+            taskElement.style.display = '';
+        } else {
+            taskElement.style.display = 'none';
+        }
+    }
 }
