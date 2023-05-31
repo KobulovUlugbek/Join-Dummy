cd38033b7355f866170d68c1165b540419e2eb78
Ulugbek Kobulov <108155304+KobulovUlugbek@users.noreply.github.com>
2023-05-31 13:25:34 +0200

    js, add select for category

--- js/add-task.js
+++ js/add-task.js
@@ -1,7 +1,10 @@
 let popup = document.getElementById('header-popup');
+let content = document.getElementById('content');
+
 
 function openPopup(){
     popup.classList.add('open-popup');
+    content.classList.add('background');
 }
 function closePopup(){
     popup.classList.remove('open-popup');
