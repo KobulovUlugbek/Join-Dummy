2f3f400ef80cd94e5a19252a58e64b1e029ee8d5
Ulugbek Kobulov <108155304+KobulovUlugbek@users.noreply.github.com>
2023-06-10 15:31:46 +0200

    added new Category Input and save function

--- js/drop-down.js
+++ js/drop-down.js
@@ -1,37 +1,65 @@
+// JSON-Array mit Kategorien
+let categories = JSON.parse(localStorage.getItem("categories")) || [
+  {
+    id: 0,
+    name: "New category",
+    circleClass: "circle-00",
+    onClick: "selectCategory(0)"
+  },
+  {
+    id: 1,
+    name: "Design",
+    circleClass: "circle-0",
+    onClick: "selectCategory(1)"
+  },
+  {
+    id: 2,
+    name: "Sales",
+    circleClass: "circle-1",
+    onClick: "selectCategory(2)"
+  },
+  {
+    id: 3,
+    name: "Backoffice",
+    circleClass: "circle-2",
+    onClick: "selectCategory(3)"
+  },
+  {
+    id: 4,
+    name: "Marketing",
+    circleClass: "circle-3",
+    onClick: "selectCategory(4)"
+  },
+  {
+    id: 5,
+    name: "Other",
+    circleClass: "circle-4",
+    onClick: "selectCategory(5)"
+  }
+];
+
 // Funktion zum Umschalten der Dropdown-Liste
 function toggleDropdown() {
-  var dropdown = document.querySelector(".overflow");
+  let dropdown = document.querySelector(".overflow");
   dropdown.classList.toggle("hidden");
 }
 
 // Funktion zum Auswählen einer Kategorie
 function selectCategory(categoryIndex) {
   // Array mit Kategorienamen
-  var categoryNames = [
-    "New category",
-    "Design",
-    "Sales",
-    "Backoffice",
-    "Marketing",
-    "Other"
-  ];
+  let categoryNames = categories.map((category) => category.name);
 
   // Array mit Klassen für die Kreise
-  var circleClasses = [
-    "circle-00",
-    "circle-0",
-    "circle-1",
-    "circle-2",
-    "circle-3",
-    "circle-4"
-  ];
+  let circleClasses = categories.map((category) => category.circleClass);
 
   // Elemente auswählen
-  var selectedCategoryText = document.getElementById("selected-category");
-  var circles = document.querySelectorAll(".drop-down-selector .subfolder .circle");
+  let selectedCategoryText = document.getElementById("selected-category");
+  let circles = document.querySelectorAll(
+    ".drop-down-selector .subfolder .circle"
+  );
 
   // Ausgewählte Kategorie festlegen
-  var selectedCategoryHTML =
+  let selectedCategoryHTML =
     categoryNames[categoryIndex] +
     '<div class="' +
     circleClasses[categoryIndex] +
@@ -39,7 +67,7 @@ function selectCategory(categoryIndex) {
   selectedCategoryText.innerHTML = selectedCategoryHTML;
 
   // Kreise entsprechend der ausgewählten Kategorie markieren
-  for (var i = 0; i < circles.length; i++) {
+  for (let i = 0; i < circles.length; i++) {
     if (i === categoryIndex) {
       circles[i].classList.add(circleClasses[i]);
     } else {
@@ -48,9 +76,85 @@ function selectCategory(categoryIndex) {
   }
 
   // Dropdown-Liste ausblenden
-  var dropdown = document.querySelector(".overflow");
+  let dropdown = document.querySelector(".overflow");
   dropdown.classList.add("hidden");
 }
+
+// Element zum Hinzufügen der Kategorien
+const overflowHiddenElement = document.querySelector(".overflow.hidden");
+
+categories.forEach((category) => {
+  overflowHiddenElement.innerHTML += `
+    <div id="subfolder-${category.id}" class="subfolder" onclick="eval('${category.onClick}')">
+      <h4>${category.name}<div class="${category.circleClass}"></div></h4>
+    </div>
+  `;
+});
+
+// Funktion zum Hinzufügen einer neuen Kategorie
+function renderNewCategory() {
+  let newCategoryInput = document.getElementById("add-new-category");
+  let newCategoryName = newCategoryInput.value;
+
+  let selectedColor = document.querySelector(".colorCategory .selected");
+  let selectedColorId = selectedColor.id;
+
+  let newCategory = {
+    id: categories.length,
+    name: newCategoryName,
+    circleClass: selectedColorId,
+    onClick: `selectCategory(${categories.length})`
+  };
+
+  categories.push(newCategory);
+  saveCategories(); // Speichere die aktualisierten Kategorien im Local Storage
+  renderCategories(); // Update the rendered categories
+  newCategoryInput.value = ""; // Reset the input field
+}
+
+function saveCategories() {
+  localStorage.setItem("categories", JSON.stringify(categories));
+}
+
+// Funktion zum Auswählen der Farbe
+function selectColor(colorId) {
+  let colorElements = document.querySelectorAll(".colorCategory div");
+
+  colorElements.forEach((element) => {
+    element.classList.remove("selected");
+  });
+
+  let selectedColor = document.getElementById(colorId);
+  selectedColor.classList.add("selected");
+}
+
+// Event Listener für Farbauswahl
+document.querySelectorAll(".colorCategory div").forEach((colorElement) => {
+  colorElement.addEventListener("click", () => {
+    selectColor(colorElement.id);
+  });
+});
+
+// Funktion zum Rendern der Kategorien
+function renderCategories() {
+  const overflowHiddenElement = document.querySelector(".overflow.hidden");
+  if (!overflowHiddenElement) return; // Exit if the element is not found
+
+  overflowHiddenElement.innerHTML = "";
+
+  categories.forEach((category) => {
+    overflowHiddenElement.innerHTML += `
+      <div id="subfolder-${category.id}" class="subfolder" onclick="${category.onClick}">
+        <h4>${category.name}<div class="${category.circleClass}"></div></h4>
+      </div>
+    `;
+  });
+}
+
+// Initial render of the categories
+renderCategories();
+
+//Assugned to
 
 function toggleDropdown2() {
   const subfolders = document.querySelectorAll(".subfolder2");
@@ -58,129 +162,3 @@ function toggleDropdown2() {
     subfolder.classList.toggle("hidden");
   });
 }
-
-function toggleCheckbox(checkboxId) {
-  const checkbox = document.getElementById(checkboxId);
-  checkbox.checked = !checkbox.checked;
-}
-
-let category = [];
-
-function renderNewCategory() {
-  let renderCategory = document.getElementById("add-new-category").value;
-  let chooseColorElement = document.querySelector(".colorCategory .selected");
-
-  if (!chooseColorElement) {
-    // Farbelement nicht gefunden oder nicht ausgewählt
-    return;
-  }
-
-  let chooseColor = chooseColorElement.style.backgroundColor;
-  let chooseColorClass = chooseColorElement.classList[0];
-
-  let newCategoryContainer = document.getElementById("new-category-container");
-  let newCategoryElement = document.createElement("div");
-  newCategoryElement.classList.add("subfolder");
-
-  // Ermittle den nächsten verfügbaren Index für die neue Kategorie
-  let categoryIndex = 0;
-  while (document.getElementById("subfolder-" + categoryIndex) !== null) {
-    categoryIndex++;
-  }
-
-  newCategoryElement.id = "subfolder-" + categoryIndex;
-  newCategoryElement.setAttribute("onclick", "selectCategory(" + categoryIndex + ")");
-
-  let newCategoryContent = document.createElement("h4");
-  newCategoryContent.textContent = renderCategory;
-
-  let newCategoryColor = document.createElement("div");
-  newCategoryColor.classList.add("selected-color");
-  newCategoryColor.classList.add(chooseColorClass);
-  newCategoryColor.style.backgroundColor = chooseColor;
-
-  newCategoryContent.appendChild(newCategoryColor);
-  newCategoryElement.appendChild(newCategoryContent);
-  newCategoryContainer.appendChild(newCategoryElement);
-
-  // Füge die neue Kategorie zum 'category'-Array hinzu
-  category.push({
-    name: renderCategory,
-    colorClass: chooseColorClass
-  });
-
-  // Auswahl der neuen Kategorie aktualisieren
-  selectCategory(categoryIndex);
-
-  // Aktualisiere die neu erstellte Kategorie im Zielbereich
-  let changeElement = document.getElementById("change");
-  changeElement.innerHTML = '<h4 id="selected-category">' + renderCategory + '<div class="' + chooseColorClass + '"></div></h4>';
-
-  // Speichere die Kategorien im Local Storage
-  saveCategoriesToLocalStorage();
-
-  // Leere das 'add-new-category' Eingabefeld
-  document.getElementById("add-new-category").value = "";
-}
-
-// Füge jedem Farbelement einen Klick-Event-Listener hinzu
-let colorElements = document.querySelectorAll(".colorCategory div");
-colorElements.forEach(function (colorElement) {
-  colorElement.addEventListener("click", function () {
-    // Entferne die Klasse 'selected' von allen Farbelementen
-    colorElements.forEach(function (element) {
-      element.classList.remove("selected");
-    });
-    // Füge die Klasse 'selected' zum angeklickten Farbelement hinzu
-    this.classList.add("selected");
-  });
-});
-
-// Warte, bis das Dokument vollständig geladen ist, bevor du die Funktion renderNewCategory() aufrufst
-document.addEventListener("DOMContentLoaded", function () {
-  renderNewCategory();
-});
-
-function saveCategoriesToLocalStorage() {
-  localStorage.setItem("categories", JSON.stringify(category));
-}
-
-
-function loadCategoriesFromLocalStorage() {
-  var savedCategories = localStorage.getItem("categories");
-  if (savedCategories) {
-    category = JSON.parse(savedCategories);
-    // Kategorien aus dem Local Storage rendern
-    renderSavedCategories();
-  }
-}
-
-function renderSavedCategories() {
-  var newCategoryContainer = document.getElementById("new-category-container");
-  // Leere den Container, um vorhandene Kategorien zu entfernen
-  newCategoryContainer.innerHTML = "";
-
-  for (var i = 0; i < category.length; i++) {
-    var categoryName = category[i].name;
-    var categoryColorClass = category[i].colorClass;
-
-    var newCategoryElement = document.createElement("div");
-    newCategoryElement.classList.add("subfolder");
-    newCategoryElement.id = "subfolder-" + i;
-    newCategoryElement.setAttribute("onclick", "selectCategory(" + i + ")");
-
-    var newCategoryContent = document.createElement("h4");
-    newCategoryContent.textContent = categoryName;
-
-    var newCategoryColor = document.createElement("div");
-    newCategoryColor.classList.add("selected-color");
-    newCategoryColor.classList.add(categoryColorClass);
-
-    newCategoryContent.appendChild(newCategoryColor);
-    newCategoryElement.appendChild(newCategoryContent);
-    newCategoryContainer.appendChild(newCategoryElement);
-  }
-}
-
-// Rufe die Funktion zum Laden der Kategorien aus dem Local Storage auf
-loadCategoriesFromLocalStorage();
